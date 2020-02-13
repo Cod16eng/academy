5ab7fd0168f0d7d737ed10b28277dc84e2ca40a2
Codrin Popescu <codrin@ascheri.co.uk>
2020-02-13 11:55:41 +0000

    modified pages and post ctrl

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :update, :destroy]
-
+  skip_before_action :require_admin, only: [:show]
   # GET /posts
   # GET /posts.json
   def index
